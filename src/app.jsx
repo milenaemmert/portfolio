d330691a02a425be21443de0baee24a073d1a000
Milena Emmert <milena.emmert@outlook.com>
2024-06-08 16:06:29 -0300

    Update project to v4.2 [Log environment]

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,18 @@
-import { MainScreen } from './screens'
-import { Header, Footer } from './components'
+import { MainScreen } from "./screens";
+import { Header, Footer } from "./components";
 
 export const App = () => {
+  console.log("MODE -> " + import.meta.env.MODE);
+  console.log("BASE_URL -> " + import.meta.env.BASE_URL);
+  console.log("PROD -> " + import.meta.env.PROD);
+  console.log("DEV -> " + import.meta.env.DEV);
+  console.log("SSR -> " + import.meta.env.SSR);
+
   return (
     <>
       <Header />
       <MainScreen />
       <Footer />
     </>
-  )
-}
+  );
+};
