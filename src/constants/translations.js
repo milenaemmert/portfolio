3bdd04f6de13e1ca91f920a09dc66f68d481b41f
milenaemmert <milena.emmert@outlook.com>
2024-10-10 01:22:22 -0300

    Update to v13 [update projetcs - images and texts]

--- src/constants/translations.js
+++ src/constants/translations.js
@@ -1,339 +1,367 @@
 export const TRANSLATIONS = {
   // COMMON
-  'Projetos': {
+  Projetos: {
     en: 'Projects',
-    es: 'Proyectos'
+    es: 'Proyectos',
   },
-  'Colaboração': {
+  Colaboração: {
     en: 'Collaboration',
-    es: 'Colaboración'
+    es: 'Colaboración',
   },
   // HEADER
   'Selecionar o idioma': {
     en: 'Select the language',
-    es: 'Seleccionar el idioma'
+    es: 'Seleccionar el idioma',
   },
   'Fechar o menu de idiomas': {
     en: 'Close the language menu',
-    es: 'Cerrar el menú de idiomas'
+    es: 'Cerrar el menú de idiomas',
   },
   'Abrir o menu de idiomas': {
     en: 'Open the language menu',
-    es: 'Abrir el menú de idiomas'
+    es: 'Abrir el menú de idiomas',
   },
   'Ir para o início': {
     en: 'Back to top',
-    es: 'Ir al inicio'
+    es: 'Ir al inicio',
   },
   'Fechar o menu principal': {
     en: 'Close the main menu',
-    es: 'Cerrar el menú principal'
+    es: 'Cerrar el menú principal',
   },
   'Abrir o menu principal': {
     en: 'Open the main menu',
-    es: 'Abrir el menú principal'
+    es: 'Abrir el menú principal',
   },
-  'Sobre': {
+  Sobre: {
     en: 'About',
-    es: 'Sobre'
+    es: 'Sobre',
   },
   'Ir para a sessão "Sobre"': {
     en: 'Go to "About"',
-    es: 'Ir a la sección "Sobre"'
+    es: 'Ir a la sección "Sobre"',
   },
-  'Conhecimento': {
+  Conhecimento: {
     en: 'Knowledge',
-    es: 'Conocimiento'
+    es: 'Conocimiento',
   },
   'Ir para a sessão "Conhecimento"': {
     en: 'Go to "Knowledge"',
-    es: 'Ir a la sección "Conocimiento"'
+    es: 'Ir a la sección "Conocimiento"',
   },
   'Ir para a sessão "Projetos"': {
     en: 'Go to "Projects"',
-    es: 'Ir a la sección "Proyectos"'
+    es: 'Ir a la sección "Proyectos"',
   },
   'Ir para a sessão "Colaboração"': {
     en: 'Go to "Collaboration"',
-    es: 'Ir a la sección "Colaboración"'
+    es: 'Ir a la sección "Colaboración"',
   },
-  'Contato': {
+  Contato: {
     en: 'Contact',
-    es: 'Contacto'
+    es: 'Contacto',
   },
   'Ir para a sessão "Contato"': {
     en: 'Go to "Contact"',
-    es: 'Ir a la sección "Contacto"'
+    es: 'Ir a la sección "Contacto"',
   },
   'Acessar o meu Behance': {
     en: 'Access my Behance',
-    es: 'Acceder a mi Behance'
+    es: 'Acceder a mi Behance',
   },
   'Acessar o meu canal no YouTube': {
     en: 'Access my YouTube channel',
-    es: 'Acceder a mi canal en YouTube'
+    es: 'Acceder a mi canal en YouTube',
   },
   'Acessar o meu GitHub': {
     en: 'Access my GitHub',
-    es: 'Acceder a mi GitHub'
+    es: 'Acceder a mi GitHub',
   },
   'Acessar o meu LinkedIn': {
     en: 'Access my LinkedIn',
-    es: 'Acceder a mi LinkedIn'
+    es: 'Acceder a mi LinkedIn',
   },
   'Acessar o meu perfil na comunidade DEV': {
     en: 'Access my DEV community profile',
-    es: 'Acceder a mi perfil en la comunidad DEV'
+    es: 'Acceder a mi perfil en la comunidad DEV',
   },
   'Acessar o meu Notion': {
     en: 'Access my Notion',
-    es: 'Acceder a mi Notion'
+    es: 'Acceder a mi Notion',
   },
   // FOOTER
   '© 2024': {
     en: '© 2024',
-    es: '© 2024'
+    es: '© 2024',
   },
   'Design e desenvolvimento por Milena Emmert': {
     en: 'Design and development by Milena Emmert',
-    es: 'Diseño y programación por Milena Emmert'
+    es: 'Diseño y programación por Milena Emmert',
   },
   'Todos os direitos reservados.': {
     en: 'All rights reserved.',
-    es: 'Todos los derechos reservados.'
+    es: 'Todos los derechos reservados.',
   },
   // MAIN ABOUT SECTION
   'Dev & Analista de Dados': {
     en: 'Dev & Data Analyst',
-    es: 'Dev & Analista de Datos'
+    es: 'Dev & Analista de Datos',
   },
   'Milena Emmert': {
     en: 'Milena Emmert',
-    es: 'Milena Emmert'
+    es: 'Milena Emmert',
   },
   'Sou analista de dados, instrutora e tenho experiência como dev front-end e conhecimento em design. Essas habilidades permitem que eu colabore de forma analítica e criativa na solução de problemas, fundamentando meu trabalho na coleta e análise de dados e na experiência do usuário.':
     {
       en: 'I am a data analyst and instructor with experience as a front-end developer and knowledge in design. These skills allow me to collaborate both analytically and creatively in problem-solving, basing my work on data collection and analysis, as well as user experience.',
-      es: 'Soy analista de datos e instructora con experiencia como desarrolladora front-end y conocimientos en diseño. Estas habilidades me permiten colaborar de manera analítica y creativa en la resolución de problemas, basando mi trabajo en la recopilación y análisis de datos y en la experiencia de los usuarios.'
+      es: 'Soy analista de datos e instructora con experiencia como desarrolladora front-end y conocimientos en diseño. Estas habilidades me permiten colaborar de manera analítica y creativa en la resolución de problemas, basando mi trabajo en la recopilación y análisis de datos y en la experiencia de los usuarios.',
     },
   'Foto da Milena Emmert, que é analista de dados, instrutora e desenvolvedora. Ela possui cabelo loiro, está usando uma boina bege e um cachecol bordô.':
     {
       en: 'Photo of Milena Emmert, who is a data analyst, instructor and developer. She has blonde hair, and is wearing a beige beret and a burgundy scarf.',
-      es: 'Foto de Milena Emmert, quien es analista de datos, instructora y desarrolladora. Tiene cabello rubio, lleva una boina beige y una bufanda burdeos.'
+      es: 'Foto de Milena Emmert, quien es analista de datos, instructora y desarrolladora. Tiene cabello rubio, lleva una boina beige y una bufanda burdeos.',
     },
   // MAIN KNOWLEDGE SECTION
-  'Design': {
+  Design: {
     en: 'Design',
-    es: 'Diseño'
+    es: 'Diseño',
   },
   'A criatividade faz parte da minha personalidade. Mas, para além do visual, meus projetos também são alinhados com as melhores práticas de UX.':
     {
       en: 'Creativity is part of my personality. However, beyond the visual aspect, my projects are also aligned with the best UX practices.',
-      es: 'La creatividad es parte de mi personalidad. Pero además del aspecto visual, mis proyectos también están alineados con las mejores prácticas de UX.'
+      es: 'La creatividad es parte de mi personalidad. Pero además del aspecto visual, mis proyectos también están alineados con las mejores prácticas de UX.',
     },
   'UI/UX': {
     en: 'UI/UX',
-    es: 'UI/UX'
+    es: 'UI/UX',
   },
-  'Web': {
+  Web: {
     en: 'Web',
-    es: 'Web'
+    es: 'Web',
   },
-  'GIMP': {
+  GIMP: {
     en: 'GIMP',
-    es: 'GIMP'
+    es: 'GIMP',
   },
-  'Canva': {
+  Canva: {
     en: 'Canva',
-    es: 'Canva'
+    es: 'Canva',
   },
-  'Illustrator': {
+  Illustrator: {
     en: 'Illustrator',
-    es: 'Illustrator'
+    es: 'Illustrator',
   },
-  'Figma': {
+  Figma: {
     en: 'Figma',
-    es: 'Figma'
+    es: 'Figma',
   },
-  'Programação': {
+  Programação: {
     en: 'Programming',
-    es: 'Programación'
+    es: 'Programación',
   },
   'Foi a minha porta de entrada na tecnologia. Todo projeto que crio, seja para design ou dados, penso de forma alinhada com os meus aprendizados de desenvolvimento.':
     {
       en: 'It was my gateway into technology. Every project I create, whether for design or data, is aligned with my development learnings.',
-      es: 'Fue mi puerta de entrada al mundo de la tecnología. Cada proyecto que creo, ya sea de diseño o datos, alineo con mis conocimientos de programación.'
+      es: 'Fue mi puerta de entrada al mundo de la tecnología. Cada proyecto que creo, ya sea de diseño o datos, alineo con mis conocimientos de programación.',
     },
-  'HTML': {
+  HTML: {
     en: 'HTML',
-    es: 'HTML'
+    es: 'HTML',
   },
-  'CSS': {
+  CSS: {
     en: 'CSS',
-    es: 'CSS'
+    es: 'CSS',
   },
-  'JavaScript': {
+  JavaScript: {
     en: 'JavaScript',
-    es: 'JavaScript'
+    es: 'JavaScript',
   },
-  'TypeScript': {
+  TypeScript: {
     en: 'TypeScript',
-    es: 'TypeScript'
+    es: 'TypeScript',
   },
-  'React': {
+  React: {
     en: 'React',
-    es: 'React'
+    es: 'React',
   },
-  'Git': {
+  Git: {
     en: 'Git',
-    es: 'Git'
+    es: 'Git',
   },
   'Análise de Dados': {
     en: 'Data Analysis',
-    es: 'Análisis de Datos'
+    es: 'Análisis de Datos',
   },
   'Os dados nos guiam. Por isso as decisões que tomo partem da coleta, análise, tratamento e compartilhamento de dados, seguindo o processo de ETL.':
     {
       en: 'Data guides us. The decisions I make are based on ETL process, which includes data collection, analysis, processing, and sharing.',
-      es: 'Los datos nos guían. Por eso, las decisiones que tomo se basan en la recopilación, análisis, procesamiento y compartición de datos, siguiendo el proceso de ETL.'
+      es: 'Los datos nos guían. Por eso, las decisiones que tomo se basan en la recopilación, análisis, procesamiento y compartición de datos, siguiendo el proceso de ETL.',
     },
-  'MySQL': {
+  MySQL: {
     en: 'MySQL',
-    es: 'MySQL'
+    es: 'MySQL',
   },
-  'Excel': {
+  Excel: {
     en: 'Excel',
-    es: 'Excel'
+    es: 'Excel',
   },
-  'Python': {
+  Python: {
     en: 'Python',
-    es: 'Python'
+    es: 'Python',
   },
   'Apps e Office Scripts': {
     en: 'Apps and Office Scripts',
-    es: 'Apps y Office Scripts'
+    es: 'Apps y Office Scripts',
   },
   'Power BI': {
     en: 'Power BI',
-    es: 'Power BI'
+    es: 'Power BI',
   },
   // MAIN PROJECTS SECTION
   'Miniatura mostrando a interface do Projeto ICO, que conta com vários controles para modificar a aparência dos ícones disponíveis na plataforma.':
     {
       en: 'Thumbnail showing the interface of ICO Project, which includes various controls to modify the appearance of the icons available on the platform.',
-      es: 'Miniatura mostrando la interfaz del Proyecto ICO, que incluye varios controles para modificar la apariencia de los iconos disponibles en la plataforma.'
+      es: 'Miniatura mostrando la interfaz del Proyecto ICO, que incluye varios controles para modificar la apariencia de los iconos disponibles en la plataforma.',
     },
   'Projeto ICO': {
     en: 'Project ICO',
-    es: 'Proyecto ICO'
+    es: 'Proyecto ICO',
   },
   'Se trata de uma plataforma de ícones visualmente atrativa, totalmente responsiva e com boas práticas de acessibilidade. Desenvolvida com HTML, CSS e JavaScript vanilla.':
     {
       en: 'It is a visually appealing icon platform, fully responsive and following good accessibility practices. Developed with HTML, CSS, and vanilla JavaScript.',
-      es: 'Es una plataforma de íconos visualmente atractiva, totalmente responsiva y con buenas prácticas de accesibilidad. Desarrollada con HTML, CSS y JavaScript vanilla.'
+      es: 'Es una plataforma de íconos visualmente atractiva, totalmente responsiva y con buenas prácticas de accesibilidad. Desarrollada con HTML, CSS y JavaScript vanilla.',
     },
+
+  'Miniatura mostrando parte da landing page desenvolvida para a Vivo, com menu, banner principal e pontos para escolher a Vivo.': {
+    en: 'Thumbnail showing part of the landing page developed for Vivo, featuring a menu, main banner, and key points on why to choose Vivo.',
+    es: 'Miniatura que muestra parte de la página de destino desarrollada para Vivo, con un menú, banner principal y puntos clave sobre por qué elegir Vivo.',
+  },
+  'React.js na Vivo': {
+    en: 'React.js for VIVO',
+    es: 'React.js en VIVO',
+  },
+  'LP desenvolvida em workshop que ministrei para a Vivo. O projeto aborda tópicos desde Componentes de Classe vs. Funcionais, até hooks avançados com TypeScript.':
+    {
+      en: 'LP developed during a workshop I led for VIVO. The project covers topics ranging from Class vs. Functional Components to advanced hooks with TypeScript.',
+      es: 'LP desarrollada durante un workshop que impartí para VIVO. El proyecto aborda temas que van desde Componentes de Clase vs. Funcionales hasta hooks avanzados con TypeScript.',
+    },
+
+  'Miniatura com dashboard de análise financeira do com gráfico candlestick e indicadores financeiros exibidos no Power BI.': {
+    en: 'Thumbnail showing a financial analysis dashboard with a candlestick chart and financial indicators displayed in Power BI.',
+    es: 'Miniatura que muestra un panel de análisis financiero con un gráfico de velas e indicadores financieros exhibidos en Power BI.',
+  },
+  'Análise de OHLC': {
+    en: 'OHLC Analysis',
+    es: 'Análisis de OHLC',
+  },
+  'Monitoramento de ativos, OHLC e indicadores financeiros com análise fundamentalista e gráfica, utilizando Power BI, Python, Yahoo Finance e Fundamentus.': {
+    en: 'Asset monitoring, OHLC, and financial indicators with fundamental and technical analysis using Power BI, Python, Yahoo Finance, and Fundamentus.',
+    es: 'Monitoreo de activos, OHLC e indicadores financieros con análisis fundamental y técnico utilizando Power BI, Python, Yahoo Finance y Fundamentus.',
+  },
+
   'Miniatura mostrando o fluxo de UX criado durante um hackathon.': {
     en: 'Thumbnail showing the UX flow created during a hackathon.',
-    es: 'Miniatura mostrando el flujo de UX creado durante un hackathon.'
+    es: 'Miniatura mostrando el flujo de UX creado durante un hackathon.',
   },
   'Hackathon UX/UI': {
     en: 'UX/UI Hackathon',
-    es: 'Hackathon de UX/UI'
+    es: 'Hackathon de UX/UI',
   },
   'Solução proposta durante um hackathon para um problema enfrentado por uma empresa de tecnologia. Foram utilizados conceitos de UX, UI e desenvolvimento front-end.':
     {
       en: 'Solution proposed in a hackathon for a problem faced by a technology company. UX, UI, and front-end development concepts were utilized.',
-      es: 'Solución propuesta durante un hackathon para un problema enfrentado por una empresa de tecnología. Se utilizaron conceptos de UX, UI y desarrollo front-end.'
+      es: 'Solución propuesta durante un hackathon para un problema enfrentado por una empresa de tecnología. Se utilizaron conceptos de UX, UI y desarrollo front-end.',
     },
   'Miniatura mostrando a interface do editor de código, que conta com a presença do código atual centralizado e algumas opções de personalização ao lado.':
     {
       en: 'Thumbnail showing the interface of the code editor, featuring the current code centered and some customization options on the side.',
-      es: 'Miniatura mostrando la interfaz del editor de código, que incluye el código actual centrado y algunas opciones de personalización a su lado.'
+      es: 'Miniatura mostrando la interfaz del editor de código, que incluye el código actual centrado y algunas opciones de personalización a su lado.',
     },
   'Editor Com Destaque de Sintaxe': {
     en: 'Syntax Highlighting Editor',
-    es: 'Editor con resaltado de sintaxis'
+    es: 'Editor con resaltado de sintaxis',
   },
   'Editor de código com destaque de sintaxe, desenvolvido com highlight.js. Também é possível salvar o código no local storage do navegador.':
     {
       en: "Code editor with syntax highlighting, developed with highlight.js. It also allows saving the code in the browser's local storage.",
-      es: 'Editor de código con resaltado de sintaxis, desarrollado con highlight.js. También es posible guardar el código en el almacenamiento local del navegador.'
+      es: 'Editor de código con resaltado de sintaxis, desarrollado con highlight.js. También es posible guardar el código en el almacenamiento local del navegador.',
     },
   'Miniatura mostrando o fluxo do protótipo de alta fidelidade criado para fins de estudo.':
     {
       en: 'Thumbnail showing the high-fidelity prototype flow created for study purposes.',
-      es: 'Miniatura mostrando el flujo del prototipo de alta fidelidad creado con fines de estudio.'
+      es: 'Miniatura mostrando el flujo del prototipo de alta fidelidad creado con fines de estudio.',
     },
   'Prototipação de UI e Design Gráfico': {
     en: 'UI Prototyping and Graphic Design',
-    es: 'Prototipación de UI y Diseño Gráfico'
+    es: 'Prototipación de UI y Diseño Gráfico',
   },
   'Protótipo de alta fidelidade com fluxos de navegação do usuário e exploração dos princípios de design gráfico e análises heurísticas.':
     {
       en: 'High-fidelity prototype with user navigation flows and exploration of graphic design principles and heuristic analyses.',
-      es: 'Prototipo de alta fidelidad con flujos de navegación del usuario y exploración de los principios de diseño gráfico y análisis heurísticos.'
+      es: 'Prototipo de alta fidelidad con flujos de navegación del usuario y exploración de los principios de diseño gráfico y análisis heurísticos.',
     },
   'Ver projeto': {
     en: 'View project',
-    es: 'Ver proyecto'
+    es: 'Ver proyecto',
   },
   // MAIN COLLABORATION SECTION
   'Tive a oportunidade de ministrar palestras e treinamentos para as seguintes empresas:':
     {
       en: "I've had the opportunity to give lectures and training sessions for the following companies:",
-      es: 'Tuve la oportunidad de dar conferencias y entrenamientos para las siguientes empresas:'
+      es: 'Tuve la oportunidad de dar conferencias y entrenamientos para las siguientes empresas:',
     },
   'Acessar o site do Boticário': {
     en: 'Access the Boticario website',
-    es: 'Acceder al sitio web del Boticário'
+    es: 'Acceder al sitio web del Boticário',
   },
   'Acessar o site da Alura': {
     en: 'Access the Alura website',
-    es: 'Acceder al sitio web de Alura'
+    es: 'Acceder al sitio web de Alura',
   },
   'Acessar o site da Ambev': {
     en: 'Access the Ambev website',
-    es: 'Acceder al sitio web de Ambev'
+    es: 'Acceder al sitio web de Ambev',
   },
   'Acessar o site da Suzano': {
     en: 'Access the Suzano website',
-    es: 'Acceder al sitio web de Suzano'
+    es: 'Acceder al sitio web de Suzano',
   },
   'Acessar o site da Globo': {
     en: 'Access the Globo website',
-    es: 'Acceder al sitio web de Globo'
+    es: 'Acceder al sitio web de Globo',
   },
   'Acessar o site da Zenvia': {
     en: 'Access the Zenvia website',
-    es: 'Acceder al sitio web de Zenvia'
+    es: 'Acceder al sitio web de Zenvia',
   },
   'Acessar o site da Algar': {
     en: 'Access the Algar website',
-    es: 'Acceder al sitio web de Algar'
+    es: 'Acceder al sitio web de Algar',
   },
   // MAIN CONTACT SECTION
   'Acesse o LinkedIn para ver minhas experiências e certificados!': {
     en: 'Access LinkedIn to see my experiences and certifications!',
-    es: '¡Accede a LinkedIn para ver mis experiencias y certificados!'
+    es: '¡Accede a LinkedIn para ver mis experiencias y certificados!',
   },
   'Vamos conversar?': {
     en: 'Shall we talk?',
-    es: '¿Vamos a conversar?'
+    es: '¿Vamos a conversar?',
   },
   '+55 51 98313-0810': {
     en: '+55 51 98313-0810',
-    es: '+55 51 98313-0810'
+    es: '+55 51 98313-0810',
   },
   'Chamar no WhatsApp': {
     en: 'Message on WhatsApp',
-    es: 'Contactar por WhatsApp'
+    es: 'Contactar por WhatsApp',
   },
   'milena.emmert@outlook.com': {
     en: 'milena.emmert@outlook.com',
-    es: 'milena.emmert@outlook.com'
+    es: 'milena.emmert@outlook.com',
   },
   'Enviar um email': {
     en: 'Send an email',
-    es: 'Enviar un email'
-  }
+    es: 'Enviar un email',
+  },
 }
--- src/hooks/use-projects.js
+++ src/hooks/use-projects.js
@@ -1,11 +1,15 @@
 import projectHackatonThumbnailFallback from '@images/main-projects/project-hackaton-thumbnail.png'
 import projectHackatonThumbnail from '@images/main-projects/project-hackaton-thumbnail.webp'
-import projectHighlightThumbnailFallback from '@images/main-projects/project-highlight-thumbnail.png'
-import projectHighlightThumbnail from '@images/main-projects/project-highlight-thumbnail.webp'
+/* import projectHighlightThumbnailFallback from '@images/main-projects/project-highlight-thumbnail.png'
+import projectHighlightThumbnail from '@images/main-projects/project-highlight-thumbnail.webp' */
 import projectIcoThumbnailFallback from '@images/main-projects/project-ico-thumbnail.png'
 import projectIcoThumbnail from '@images/main-projects/project-ico-thumbnail.webp'
-import projectPrototypeThumbnailFallback from '@images/main-projects/project-prototype-thumbnail.png'
-import projectPrototypeThumbnail from '@images/main-projects/project-prototype-thumbnail.webp'
+/* import projectPrototypeThumbnailFallback from '@images/main-projects/project-prototype-thumbnail.png'
+import projectPrototypeThumbnail from '@images/main-projects/project-prototype-thumbnail.webp' */
+import projectVivoThumbnailFallback from '@images/main-projects/project-vivo-thumbnail.png'
+import projectVivoThumbnail from '@images/main-projects/project-vivo-thumbnail.webp'
+import projectOHLCThumbnailFallback from '@images/main-projects/project-ohlc-thumbnail.png'
+import projectOHLCThumbnail from '@images/main-projects/project-ohlc-thumbnail.webp'
 
 import { useTranslation } from '@hooks'
 
@@ -29,6 +33,34 @@ export const useProjects = () => {
     },
     {
       thumbnail: {
+        src: projectVivoThumbnail,
+        fallback: projectVivoThumbnailFallback,
+        alt: t(
+          'Miniatura mostrando parte da landing page desenvolvida para a Vivo, com menu, banner principal e pontos para escolher a Vivo.'
+        )
+      },
+      title: t('React.js na Vivo'),
+      description: t(
+        'LP desenvolvida em workshop que ministrei para a Vivo. O projeto aborda tópicos desde Componentes de Classe vs. Funcionais, até hooks avançados com TypeScript.'
+      ),
+      link: 'https://vivo-six.vercel.app/'
+    },
+    {
+      thumbnail: {
+        src: projectOHLCThumbnail,
+        fallback: projectOHLCThumbnailFallback,
+        alt: t(
+          'Miniatura com dashboard de análise financeira do com gráfico candlestick e indicadores financeiros exibidos no Power BI.'
+        )
+      },
+      title: t('Análise de OHLC'),
+      description: t(
+        'Monitoramento de ativos, OHLC e indicadores financeiros com análise fundamentalista e gráfica, utilizando Power BI, Python, Yahoo Finance e Fundamentus.'
+      ),
+      link: 'https://app.powerbi.com/view?r=eyJrIjoiYTcyYTE1ZjAtNDYzZC00YzhjLWFjMTgtZWQyMzY1ODhlMWRlIiwidCI6ImQ2YjY4NTBhLWIzNWUtNGZkYS04YjViLTQ0NjEyZGY5NTY5ZiJ9'
+    },
+    {
+      thumbnail: {
         src: projectHackatonThumbnail,
         fallback: projectHackatonThumbnailFallback,
         alt: t('Miniatura mostrando o fluxo de UX criado durante um hackathon.')
@@ -39,7 +71,7 @@ export const useProjects = () => {
       ),
       link: 'https://www.figma.com/board/ne9XFWNsYL3UWzNo3PnN7P/Hackathon-FCamara-2021'
     },
-    {
+    /* {
       thumbnail: {
         src: projectHighlightThumbnail,
         fallback: projectHighlightThumbnailFallback,
@@ -52,7 +84,7 @@ export const useProjects = () => {
         'Editor de código com destaque de sintaxe, desenvolvido com highlight.js. Também é possível salvar o código no local storage do navegador.'
       ),
       link: 'https://milenaemmert.github.io/alura-challenge-frontend/index.html'
-    },
+    }, 
     {
       thumbnail: {
         src: projectPrototypeThumbnail,
@@ -66,6 +98,6 @@ export const useProjects = () => {
         'Protótipo de alta fidelidade com fluxos de navegação do usuário e exploração dos princípios de design gráfico e análises heurísticas.'
       ),
       link: 'https://www.figma.com/design/pTrubNaItW6ZUUTk8QmiLQ/Local-Imo-App'
-    }
+    }*/
   ]
 }
